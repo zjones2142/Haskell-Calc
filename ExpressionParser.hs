d665b465039462d99b4d0a85a171d0dd605cbfbd
zjones2142 <zachjones2142@gmail.com>
2023-07-19 10:56:41 -0500

    fix imports and begin writing parser

--- ExpressionParser.hs
+++ ExpressionParser.hs
@@ -1,14 +1,17 @@
 import Text.Parsec (ParseError)
-import Text.Parsec.String (Parser)
-import Text.Parsec.String.Parsec (try)
-import Text.Parsec.String.Char (oneOf, char, digit, satisfy)
-import Text.Parsec.String.Combinator (many1, choice, chainl1)
+import Text.Parsec.String ( Parser )
+import Text.Parsec.Char (oneOf, char, digit, satisfy)
+import Text.Parsec.Combinator (many1, choice, chainl1)
 import Control.Applicative ((<|>), many)
 import Control.Monad (void)
 import Data.Char (isLetter, isDigit)
-import FunctionsAndTypesForParsing
 
 num :: Parser Integer
 num = do 
     n <- many1 digit
     return (read n)
+
+--var :: Parser char
+--var = do
+    --x <- many1 char
+    --return (read x)
