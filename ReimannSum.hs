c19ed458e844d13e1602e1c626d32607c8c99ebc
zjones2142 <zachjones2142@gmail.com>
2023-08-09 12:11:00 -0500

    removal of parser (ghci bindings can be used)

--- ReimannSum.hs
+++ ReimannSum.hs
@@ -1,12 +1,19 @@
 --import ExpressionParser
 
---derive :: (a -> b) -> (a -> b)
---derive = 
+--Parse equation, 
+--calculate x values, 
+--pass xs into equation to generate y values,
+--find area of all rectangles, 
+--find sum of rectangles
 
---eval' :: (a -> b) -> [a] -> [(a,b)]
---eval' f xs = map f xs
+--evaluate :: 
 
---evaluate :: (a,b,c,d) -> b
---evaluate (f:dx:t:r) = approximation
+genXs :: (Float,Float) -> Float -> [Float]
+genXs (a,b) c = map (a +) [(0*c),(1*c)..] --c is size of rectangle, (a,b) is range of x values
+--always pass in first x value as 'a' rather than the 'a' in range so the endpoint type is correct
+--ps: use 'take' when grabbing values from infinite list
+
+--reimann :: (a,b,c,d) -> b
+--reimann (f:dx:t:r) = approximation --f is function, dx is number of rectangles, r is range in form of tuple 
 --  let approximation = sum rectangles
 --  let rectangles = 
